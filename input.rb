1bb3a29b26aaa8897def299a84a23210a010af88
Broc Nickodemus <Broc Nickodemus>
2016-10-21 19:48:31 -0700

    added files

---
+++ input.rb
@@ -0,0 +1,33 @@
+# basic loops and input
+
+print "What's your first name?"
+first_name=gets.chomp
+first_name=first_name.capitalize!
+print "What's your last name?"
+last_name=gets.chomp
+last_name=last_name.capitalize!
+print "What's your city?"
+city=gets.chomp
+city=city.capitalize!
+print "What's your state?"
+state=gets.chomp
+state=state.upcase!
+puts "#{first_name}"
+puts "#{last_name}"
+puts "#{city}"
+puts "#{state}"
+
+30.times do |x|
+	    print "Ruby!"
+end
+
+array = [1,2,3,4,5]
+
+array.each do |x|
+	  x += 10
+	    print "#{x}"
+end
+
+for num in 1...10
+	  puts num
+end
